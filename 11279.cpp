5b899186e208c9128d7ad0306d74f4e22a3d040a
hellojihyoung <jihyoungkwon@gmail.com>
2020-02-17 15:13:03 +0900

    Baekjoon :: PriorityQueue_STL

---
+++ 11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	cin.tie(0);
+	cin.sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	priority_queue <int> queue;
+
+	int x;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if ((x == 0) && queue.empty()) 
+			cout << "0\n";
+		else if (x != 0) {
+			queue.push(x);
+		}
+		else {
+			cout << queue.top() << "\n";
+			queue.pop();
+		}
+	}
+	return 0;
+}
