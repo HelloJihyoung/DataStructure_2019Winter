ba87fd3ba7a7178539dddb8a1e5bd44a2721dd68
Jihyoung <emili1001@naver.com>
2021-01-12 18:00:29 +0900

    210112 solved

---
+++ Programmers/Level2/카펫.py
@@ -0,0 +1,32 @@
+def solution(brown, yellow):
+    answer = []
+    decimal = []
+    decimal_back =[]
+    
+    
+    for i in range(1, int(yellow**(1/2)) + 1): 
+        if yellow % i == 0:
+            decimal.append(i)
+            if (i != (yellow // i)): 
+                decimal_back.append(yellow//i)
+
+    decimal = (decimal + decimal_back[::-1])[::-1]
+
+
+    for i in range(len(decimal)):
+
+        check = (decimal[i] * 2) + (yellow/decimal[i] * 2) + 4
+
+        if brown == check:
+            answer.append(decimal[i] + 2)
+            answer.append(yellow/decimal[i] +2)
+            break
+
+    return answer
+
+
+
+brown = 24
+yellow = 24
+
+print(solution(brown, yellow))
