c38f6648752c9cf2d4331ef8fb5be64f6252e61d
Jihyoung <emili1001@naver.com>
2021-01-20 23:26:39 +0900

    210121 unsolved

---
+++ Programmers/Level2/삼각달팽이.py
@@ -0,0 +1,34 @@
+# unsolved
+def solution(n):
+    space = [[0 for col in range(n)] for row in range(n)]
+    # matrix = [[0] * _ for _ in range(1, n+1)]
+
+    # j = 0
+    # i = 0
+    # num = 1
+
+
+    # for k in range(1, n+1):
+    #     if k % 3 == 1:
+    #         while True:
+    #             matrix[i][j] = num
+    #             num += 1
+    #             i += 1
+    #             if i >= n or matrix[i][j]:
+    #                 j -= 1
+    #                 k += 1
+    #                 break
+                
+        
+
+    # for x, y, z, f in matrix:   
+    #     print(x, y, z, f)
+    #a = [[1], [2, 9], [3, 10, 8], [4, 5, 6, 7]]
+    #print(a[3][3])
+    
+    answer = []
+    return answer
+
+
+n = 4
+print(solution(n))
