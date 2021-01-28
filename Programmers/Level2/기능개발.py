14935833ab0ae63d14167eaf0b317df335406abf
Jihyoung <emili1001@naver.com>
2021-01-28 13:16:31 +0900

    210126 solved

--- Programmers/Level2/기능개발.py
+++ Programmers/Level2/기능개발.py
@@ -1,33 +1,28 @@
 import math
 
+
 def solution(progresses, speeds):
     term = []
     answer = []
 
-    for i in range (len(progresses)):
+    for i in range(len(progresses)):
         cal = math.ceil(float(100-progresses[i]) / speeds[i])
         term.append(cal)
 
     print(term)
     # [5, 10, 1, 1, 20, 1]
     cnt = 1
-    while len(term) > 0:
-        front = term.pop(0)
-        for i in term:            
-            if front > i:
-                cnt += 1
-                break
-            else:
-                if len(term) == 1 and front < i:
-                    cnt += 1
-                    break
-                answer.append(cnt)
-                cnt = 1    
-        
+   
+    for i in range(1,  len(term)):
+        if term[i-cnt] >= term[i]:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            cnt = 1
+    answer.append(cnt)
 
     print(answer)
     return answer
-
 
 
 progresses = [95, 90, 99, 99, 80, 99]
