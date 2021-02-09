125675f0e31770e5112593f4c33ceb48e942d6a1
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-02-09 23:42:21 +0900

    못했어,, 못했다구,,

---
+++ Programmers/Level2/가장 큰 정사각형 찾기.py
@@ -0,0 +1,10 @@
+def solution(board):
+    answer = 1234
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+
+
+    return answer
+
+board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+print(solution(board))
