2973f0055eef4792d33b132f25120a02ab252119
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-02-12 22:33:26 +0900

    trying

--- Programmers/Level2/가장 큰 정사각형 찾기.py
+++ Programmers/Level2/가장 큰 정사각형 찾기.py
@@ -1,10 +1,12 @@
 def solution(board):
     answer = 1234
-    for i in range(1, len(board)):
-        for j in range(1, len(board[0])):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if (i > 0) and (j > 0) and (board[i][j] != 0):
+                board[i][j] = min(board[i][j-1], board[i-1][j], board[i-1][j-1])+1
+            answer = max(answer, board[i][j])
+    return answer ** 2
 
-
-    return answer
 
 board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
 print(solution(board))
