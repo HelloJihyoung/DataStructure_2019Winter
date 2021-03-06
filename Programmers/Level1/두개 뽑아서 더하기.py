f924e2ca62ec0542c1efc47165651866640c6f63
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-03-07 00:18:29 +0900

    solved 210306

--- Programmers/Level1/두개 뽑아서 더하기.py
+++ Programmers/Level1/두개 뽑아서 더하기.py
@@ -1,12 +1,10 @@
 def solution(numbers):
-    answer = []
-    numbers.sort()
-    print(numbers)
-    for i in numbers:
-        for j in numbers:
-            if i + j not in answer:
-                answer.append(i+j)
-    return answer
+    answer = set()
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.add(numbers[i] + numbers[j])
+
+    return sorted(list(answer))
 
 
 numbers = [2,1,3,4,1]
