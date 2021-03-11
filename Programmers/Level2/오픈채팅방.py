b08c3d213e1b127cbc16cda228c5b8b3a8867f00
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-03-11 21:35:42 +0900

    solved 210311

--- Programmers/Level2/오픈채팅방.py
+++ Programmers/Level2/오픈채팅방.py
@@ -13,19 +13,6 @@ def solution(record):
             answer.append(logInfo[arrSplit[1]] + '님이 들어왔습니다.' )
         elif arrSplit[0] == "Leave":
             answer.append(logInfo[arrSplit[1]] + '님이 나갔습니다.' )
-        # else:
-        #     pass
-        
-    
-    # for mes in record: 
-    #     if mes.split(' ')[0] == 'Enter': 
-    #         # userdict[mes.split(' ')[1]] + '님이 들어왔습니다.' 이런식으로 해도 가능 
-    #         answer.append("{}님이 들어왔습니다.".format(logInfo[mes.split(' ')[1]]))
-    #     elif mes.split(' ')[0] == 'Leave': 
-    #         answer.append("{}님이 나갔습니다.".format(logInfo[mes.split(' ')[1]]))
-    #     else:
-    #         pass
-
     
     return answer
 
